419a812140e4f45f4efe8aa3fab91cca599d6754
k3vur <theonlyulti@gmail.com>
2026-05-21 17:31:44 +0200

    schema changes

--- src/domain/schema.ts
+++ src/domain/schema.ts
@@ -2,62 +2,44 @@ import { z } from "zod";
 import { zt } from "zod-temporal";
 
 const BaseWorkout = z.object({
+  id: z.string(),
   title: z.string(),
-  durationSeconds: z.number(),
-  description: z.string().optional(),
+  duration: zt.duration(),
 });
 
-export const YouTubeWorkoutSchema = BaseWorkout.extend({
+export const YouTubeWorkout = BaseWorkout.extend({
   type: z.literal("youtube"),
   video_id: z.string().min(1, "Video ID is required"),
 });
 
-export const Workout = z.discriminatedUnion("type", [YouTubeWorkoutSchema]);
+export const Workout = z.discriminatedUnion("type", [YouTubeWorkout]);
 
 /**
  * Global list of all offered Workouts
  */
-export const WorkoutPlan = z.array(Workout);
+export const AvailableWorkouts = z.array(Workout);
 
 /**
  * Selected Workouts for a day
  */
-export const WorkoutSelection = z.object({
+export const PlannedWorkout = z.object({
+  id: z.int(),
+  workout: Workout,
   day: zt.plainDate(),
-  workouts: z.array(Workout),
 });
 
 /**
  * Finished Workout
  */
 export const FinishedWorkout = z.object({
+  id: z.int(),
   workout: Workout,
   timestamp: zt.instant(),
 });
 
-export const FinishedWorkoutSession = z.object({
-  day: zt.plainDate(),
-  finishedWorkouts: z.array(FinishedWorkout),
-});
-
 // Export inferred types
+export type YouTubeWorkout = z.infer<typeof YouTubeWorkout>;
 export type Workout = z.infer<typeof Workout>;
-export type WorkoutPlan = z.infer<typeof WorkoutPlan>;
-export type WorkoutSelection = z.infer<typeof WorkoutSelection>;
+export type AvailableWorkouts = z.infer<typeof AvailableWorkouts>;
+export type PlannedWorkout = z.infer<typeof PlannedWorkout>;
 export type FinishedWorkout = z.infer<typeof FinishedWorkout>;
-export type FinishedWorkoutSession = z.infer<typeof FinishedWorkoutSession>;
-
-// LocalStorage Schema
-const LocalStorageSchema = z.object({
-  /**
-   * reverse-chronologically ordered
-   */
-  selectedWorkouts: z.array(WorkoutSelection),
-
-  /**
-   * reverse-chronologically ordered
-   */
-  finishedWorkouts: z.array(FinishedWorkout),
-});
-
-export type LocalStorage = z.infer<typeof LocalStorageSchema>;
