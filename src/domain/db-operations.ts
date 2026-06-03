107d7686f053d3f5ec214046bde93f0473bff958
k3vur <theonlyulti@gmail.com>
2026-06-03 12:31:45 +0200

    workouts page wip

--- src/domain/db-operations.ts
+++ src/domain/db-operations.ts
@@ -63,3 +63,42 @@ export function useSuggestedWorkouts(day: Temporal.PlainDate) {
       .where(({ workout }) => not(inArray(workout.id, excludedWorkouts))),
   );
 }
+
+export async function finishWorkout(
+  timestamp: Temporal.Instant,
+  workout: string | Workout,
+): Promise<void> {
+  const workoutId = typeof workout === "string" ? workout : workout.id;
+  finishedWorkoutsCollection.insert({ timestamp, workoutId });
+}
+
+export function finishedWorkoutsQuery(day: Temporal.PlainDate) {
+  return new Query()
+    .from({ finishedWorkout: finishedWorkoutsCollection })
+    .join(
+      { workout: availableWorkoutsCollection },
+      ({ finishedWorkout, workout }) => eq(finishedWorkout.workoutId, workout.id),
+      "inner",
+    )
+    .where(({ finishedWorkout }) =>
+      and(
+        gte(finishedWorkout.timestampAsDate, Temporals.Utils.plainDateToDate(day)),
+        lt(finishedWorkout.timestampAsDate, Temporals.Utils.plainDateToDate(day.add({ days: 1 }))),
+      ),
+    )
+    .select(({ finishedWorkout, workout }) => ({ ...finishedWorkout, workout }));
+}
+
+export function useTodaysWorkout(day: Temporal.PlainDate) {
+  const { data: finishedWorkouts } = useLiveQuery({ query: finishedWorkoutsQuery(day) });
+  return useLiveQuery({
+    query: plannedWorkoutsQuery(day).where(({ plannedWorkout }) =>
+      not(
+        inArray(
+          plannedWorkout.workoutId,
+          finishedWorkouts.map((fw) => fw.workoutId),
+        ),
+      ),
+    ),
+  });
+}
--- src/routes/workout.tsx
+++ src/routes/workout.tsx
@@ -1,9 +1,65 @@
+import { inArray, not, useLiveQuery } from "@tanstack/react-db";
 import { createFileRoute } from "@tanstack/react-router";
+
+import { PageTitle } from "#/components/layout/page-title";
+import { Button } from "#/components/ui/button";
+import { YouTubeWorkoutCard } from "#/components/ui/youtube-workout-card";
+
+import {
+  FinishedWorkout,
+  finishedWorkoutsQuery,
+  finishWorkout,
+  plannedWorkoutsQuery,
+  type PlannedWorkout,
+  type Workout,
+} from "../domain";
 
 export const Route = createFileRoute("/workout")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/workout"!</div>;
+  const today = Temporal.Now.plainDateISO();
+  const { data: finishedWorkouts } = useLiveQuery({ query: finishedWorkoutsQuery(today) });
+  const finishedWorkoutIds = finishedWorkouts.map((fw) => fw.workoutId);
+  const { data: plannedWorkouts } = useLiveQuery({
+    query: plannedWorkoutsQuery(today).where(({ plannedWorkout }) =>
+      not(inArray(plannedWorkout.workoutId, finishedWorkoutIds)),
+    ),
+  });
+
+  return (
+    <WorkoutPage
+      plannedWorkouts={plannedWorkouts}
+      finishedWorkouts={finishedWorkouts}
+      onFinishWorkout={(workout) => finishWorkout(Temporal.Now.instant(), workout)}
+    />
+  );
+}
+
+type WorkoutPageProps = Readonly<{
+  plannedWorkouts: (PlannedWorkout & { workout: Workout })[];
+  finishedWorkouts: (FinishedWorkout & { workout: Workout })[];
+  onFinishWorkout: (workout: Workout) => void;
+}>;
+
+function WorkoutPage({ plannedWorkouts, onFinishWorkout }: WorkoutPageProps) {
+  return (
+    <>
+      {plannedWorkouts.length === 0 ? (
+        <div>You Pumped it 💪</div>
+      ) : (
+        <>
+          <PageTitle>Todays Workout</PageTitle>
+          <div>
+            {plannedWorkouts.map((pw) => (
+              <YouTubeWorkoutCard workout={pw.workout} key={pw.workoutId}>
+                <Button onClick={() => onFinishWorkout(pw.workout)}>Finish Workout</Button>
+              </YouTubeWorkoutCard>
+            ))}
+          </div>
+        </>
+      )}
+    </>
+  );
 }
